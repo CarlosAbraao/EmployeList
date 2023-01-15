96bac93ab1fa90722b88b13477f32362f1ee3122
Carlos Abraao <carloschavesnd@hotmail.com>
2023-01-15 14:35:12 -0300

    inicio do projeto com banco de dados mysql

---
+++ src/main/java/com/devcarlos/employe/CrudPaginacaoThymeApplication.java
@@ -0,0 +1,13 @@
+package com.devcarlos.employe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPaginacaoThymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPaginacaoThymeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devcarlos/employe/CrudPaginacaoThymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcarlos.employe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPaginacaoThymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
