24d84d4035c127175a93ac45196e1772158f0522
Carlos Abraao <carloschavesnd@hotmail.com>
2023-01-23 18:41:21 -0300

    interacao do controller com o thymeleaf/registro.html

--- src/main/java/com/devcarlos/employe/controller/UserRegistrationController.java
+++ src/main/java/com/devcarlos/employe/controller/UserRegistrationController.java
@@ -2,7 +2,9 @@ package com.devcarlos.employe.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.devcarlos.employe.dto.UserRegistrationDto;
@@ -15,7 +17,22 @@ public class UserRegistrationController {
 	@Autowired
 	private UserService userService; 
 
+	//CRIANDO UM OBJETO DE FORMA DIFERENTE ESSE OBJETO VAI INTERAGIR COM O THYMELEAF
+	@ModelAttribute("user")
+	public UserRegistrationDto userRegistrationDto() {
+		return new UserRegistrationDto();
+	}
 	
+	@GetMapping
+	public String showRegistrationForm() {
+		
+		
+		
+		return "registration";
+	}
+	
+	
+	@PostMapping
 	public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
 		userService.save(registrationDto);
 		return "redirect:/registration?success";
